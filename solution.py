7988e9acd342f7054d1e71a8c2f48a6a675ee02b
tetaden <84914528+tetaden@users.noreply.github.com>
2023-04-13 13:21:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from hyppo.ksample import MMD
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 415542660 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    ans = MMD(compute_kernel="rbf", gamma=1/10).test(x, y)[1] < 0.04
+    return ans # Ваш ответ, True или False
